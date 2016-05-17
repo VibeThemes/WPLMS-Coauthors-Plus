1133ee83afde8148b9e93e616beeeaad5bf38567
Garrett Hyder <garrett@eclipsecreative.ca>
2016-05-17 11:04:04 -0700

    Reverted Version, sorry that should be handle when pushing to WP SVN

--- wplms-coauthor-plus.php
+++ wplms-coauthor-plus.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://www.vibethemes.com/
  * Description: Integrates CoAuthor Plus with WPLMS
  * Author: VibeThemes
- * Version: 1.1.1
+ * Version: 1.1
  * Author URI: https://vibethemes.com/
  * License: GNU AGPLv3
  * License URI: http://www.gnu.org/licenses/agpl-3.0.html
